3d385682363acc38bb682a4e4d80042bdc8c29dd
Sunxyw <git@sunxyw.xyz>
2023-11-30 01:00:29 +0800

    use oracledb instead of cx_oracle

--- src/db.py
+++ src/db.py
@@ -1,19 +1,19 @@
-import cx_Oracle
-from cx_Oracle import Connection, SessionPool
+import oracledb
+from oracledb import Connection
 
 from src import OracleAccount
 
 
-def create_connection_pool() -> SessionPool:
-    return cx_Oracle.SessionPool(user=OracleAccount.Oracle_Database_Account_Username,
-                                 password=OracleAccount.Oracle_Database_Account_Password,
-                                 dsn=cx_Oracle.makedsn("studora.comp.polyu.edu.hk", 1521, sid="dbms"),
-                                 encoding="UTF-8",
-                                 min=2, max=5, increment=1,
-                                 threaded=True)
+def create_connection_pool():
+    return oracledb.create_pool(user=OracleAccount.Oracle_Database_Account_Username,
+                                password=OracleAccount.Oracle_Database_Account_Password,
+                                dsn=oracledb.makedsn("studora.comp.polyu.edu.hk", 1521, sid="dbms"),
+                                encoding="UTF-8",
+                                min=2, max=5, increment=1,
+                                threaded=True)
 
 
-def get_connection() -> Connection:
+def get_connection():
     return pool.acquire()
 
 
