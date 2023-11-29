ecd6386203b8c4a6ec921767c9818bc184179a5c
Rick Astley <email@example.com>
2023-11-30 00:46:30 +0800

    added test function and added account

---
+++ src/OracleAccount.py
@@ -0,0 +1,4 @@
+# Hello
+# Still better than a broken door
+Oracle_Database_Account_Username = str("\"22083184d\"")
+Oracle_Database_Account_Password = str("cqpncltc")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,36 @@
-import cx_Oracle
+import oracledb
+import OracleAccount
 
-userpwd = "cqpncltc" # Obtain password string from a user prompt or environment variable
 
-poly = cx_Oracle.makedsn("studora.comp.polyu.edu.hk", 1521, sid="DBMS")
 
-connection = cx_Oracle.connect(user="\"22081384d\"@dbms", password=userpwd,
-                               dsn=poly,
-                               encoding="UTF-8")
 
-print("Successfully connected to Oracle Database")
 
+
+
+def Oracle_Database_connection():
+    connection = oracledb.connect(user=OracleAccount.Oracle_Database_Account_Username, password=OracleAccount.Oracle_Database_Account_Password,
+                                host="studora.comp.polyu.edu.hk", port=1521, sid="dbms",
+                                encoding="UTF-8")
+
+
+    print("Successfully connected to Oracle Database")
+    return connection
+
+
+
+def main():
+    connection = Oracle_Database_connection()
+    cursor = connection.cursor()
+    
+
+    cursor.execute("insert into USERS values (1, 'A', 'AA', 'BB', 'N')")
+    connection.commit()
+
+    for result in cursor.execute("select * from USERS"):
+        print(result)
+
+    connection.close()
+
+if __name__ == "__main__":
+    main()
+    
